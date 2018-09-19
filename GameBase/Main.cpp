6bfd84c84d18908af7d062745d25dd5ee6d919db
Sage King <sogstheking@gmail.com>
2018-09-19 16:22:41 -0600

    Start of a Direct3d renderer

--- GameBase/Main.cpp
+++ GameBase/Main.cpp
@@ -1,6 +1,49 @@
+#include <Windows.h>
+
 int main()
 {
+	m_rc;
+	int x = CW_USEDEFAULT;
+	int y = CW_USEDEFAULT;
 
+	m_hMenu = NULL;
+
+	int nDefaultWidth = 640;
+	int nDefaultHeight = 480;
+
+	if (m_hInstance == NULL)
+		m_hInstance = (HINSTANCE)GetModuleHandle(NULL);
+
+	HICON hIcon = NULL;
+	WCHAR szExePath[MAX_PATH];
+	GetModuleFileName(NULL, szExePath, MAX_PATH);
+
+	if (hIcon == NULL)
+		hIcon = ExtractIcon(m_hInstance, szExePath, 0);
+
+	WNDCLASS wndClass;
+	wndClass.style = CS_DBLCLKS;
+	wndClass.lpfnWndProc = Main::StaticWindowProc;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hInstance = m_hInstance;
+	wndClass.hIcon - hIcon;
+	wndClass.hCursor - LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = m_windowClassName.c_str();
+
+	if (!RegisterClass(&wndClass))
+	{
+		DWORD dwError = GetLastError();
+		if (dwError != ERROR_CLASS_ALREADY_EXISTS)
+			return HRESULT_FROM_WIN32(dwError);
+	}
+
+	SetRect(&m_rc, 0, 0, nDefaultWidth, nDefaultHeight);
+	AdjustWindowRect(&m_rc, WS_OVERLAPPEDWINDOW, (m_hMenu != NULL) ? true : false);
+
+	m_hWnd - CreateWindwo()
 
 
 }
