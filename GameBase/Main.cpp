bcba4a553edecd84f46f490f1636606d46771fc5
Sage King <sogstheking@gmail.com>
2018-09-26 16:43:21 -0600

    Move to DirectXTutorial.com's code.  Thankfully.

--- GameBase/Main.cpp
+++ GameBase/Main.cpp
@@ -1,9 +1,71 @@
-#include "Window.h"
+#include <Windows.h>
+#include <windowsx.h>
 
-int main()
+LRESULT CALLBACK WindowProc(
+	HWND hWnd,
+	UINT message,
+	WPARAM wParam,
+	LPARAM lParam);
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	Window window;
+	HWND hWnd;
+	WNDCLASSEX wc;
 
-	window.createWindow();
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpszClassName = "WindowClass1";
+
+	RegisterClassEx(&wc);
+
+	hWnd = CreateWindowEx(
+		NULL,
+		"WindowClass1",
+		"Our First Windowed Program",
+		WS_OVERLAPPEDWINDOW,
+		300,
+		300,
+		500,
+		400,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	MSG msg;
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (message)
+	{
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+		return 0;
+	}
+	break;
+	}
+	return DefWindowProc(hWnd, message, wParam, lParam);
 
 }
